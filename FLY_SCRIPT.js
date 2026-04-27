d8b9ca4e5ea840ffb5347166bcc3aa5f6dec5789
dhirajdhar <dhirajrohit.exe@gmail.com>
2026-04-27 19:08:52 +0100

    Add levitation script with key controls

---
+++ FLY_SCRIPT.js
@@ -0,0 +1,69 @@
+// these hacks are very good and made are by the creator dhar games himself me dd hiii :DDDDDDDD
+
+let flying = false;
+alert("Successfully activated levitation script");
+document.addEventListener('keydown', async e => {
+  if (e.key.toLowerCase() === '[' && !keyPressedOnce['[']) {
+    keyPressedOnce['['] = true;
+    flying = true;
+    GRAVITY = 0;
+    localPlayer.y = localPlayer.y + 10;
+    alert("Flying cheats enabled");
+  }
+});
+
+document.addEventListener('keyup', e => {
+  if (e.key.toLowerCase() === '[') {
+    keyPressedOnce['['] = false;
+  }
+});
+
+document.addEventListener('keydown', async e => {
+  if (e.key.toLowerCase() === ']' && !keyPressedOnce[']']) {
+    keyPressedOnce[']'] = true;
+    flying = false;
+    alert("Flying cheats disabled");
+  }
+});
+
+document.addEventListener('keyup', e => {
+  if (e.key.toLowerCase() === ']') {
+    keyPressedOnce[']'] = false;
+  }
+});
+
+document.addEventListener('keydown', async e => {
+  if (e.key.toLowerCase() === 'q' && !keyPressedOnce['q']) {
+    keyPressedOnce['q'] = true;
+    if (flying === true) {
+      GRAVITY = 30;
+    }
+  }
+});
+
+document.addEventListener('keyup', e => {
+  if (e.key.toLowerCase() === 'q') {
+    keyPressedOnce['q'] = false;
+    if (flying === true) {
+      GRAVITY = 0;
+    }
+  }
+});
+
+document.addEventListener('keydown', async e => {
+  if (e.key.toLowerCase() === 'e' && !keyPressedOnce['e']) {
+    keyPressedOnce['e'] = true;
+    if (flying === true) {
+      GRAVITY = -400;
+    }
+  }
+});
+
+document.addEventListener('keyup', e => {
+  if (e.key.toLowerCase() === 'e') {
+    keyPressedOnce['e'] = false;
+    if (flying === true) {
+      GRAVITY = 0;
+    }
+  }
+});
